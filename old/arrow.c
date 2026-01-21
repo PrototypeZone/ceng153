35c2aaa1b19be52ea74e18e3b1ead86784560757
krismedri <122123762+krismedri@users.noreply.github.com>
2026-01-21 18:03:34 -0500

    Add files via upload

---
+++ old/arrow.c
@@ -0,0 +1,73 @@
+/** @brief Example of sense-hat usage
+ *  @file arrow.c
+ *  @since 2026-01-21
+ *
+ *  compile by typing gcc arrow.c led2472g.c lsm9ds1.c -li2c -o arrow
+ *             
+ *  run by typing ./arrow
+ */
+
+#include "arrow.h"
+
+/** Development of digital compass
+ * @author Kris Medri
+ * @since 2026-01-21
+ * C code Demonstration of Astro Pi Sense Hat.
+ */
+int main(void) 
+{
+    // Output 
+    fprintf(stdout,"Unit: %LX\n",ShGetSerial());
+    struct fb_t *fb;
+    fb=ShInit(fb);
+    memset(fb, 0, 128);
+    usleep (300000);
+
+    //shtest
+    uint16_t logo[8][8] = { HB,HB,HB,HB,HB,HB,HB,HB,
+			HB,HB,HW,HB,HB,HW,HB,HY,
+			HB,HB,HW,HB,HB,HW,HY,HY,
+			HB,HB,HW,HB,HB,HW,HY,HY,
+			HB,HB,HW,HW,HW,HW,HY,HY,
+			HB,HB,HW,HY,HY,HW,HY,HY,
+			HB,HY,HW,HY,HY,HW,HY,HY,
+			HY,HY,HY,HY,HY,HY,HY,HY,
+    };
+    ShViewPattern(logo,fb);
+    usleep (300000);
+    char str[33];
+    sprintf(str,"T: mB");
+    ShViewMessage(str,100,WHITE,BLACK,fb);
+
+    return EXIT_SUCCESS;
+}
+
+/** @brief Converts a character to an LED matrix pattern.
+ */
+void ShConvertCharacterToPattern(char c, uint16_t image[8][8], uint16_t colorText, uint16_t colorBackground)
+{
+    int i=0;
+    int j,k;
+    int tailleTableDeConvertion=sizeof(font)/sizeof(Tfont);
+
+    // Recherche si le caractere existe dans la table de convertion (cf font.h)
+    while(c!=font[i].caractere && i < tailleTableDeConvertion )
+	i++;
+
+    // Si le caractere est dans la table on le converti
+    if(i < tailleTableDeConvertion)
+    {
+        for (j=0;j<8;j++)
+        {
+            for(k=0;k<8;k++)
+            {
+                if(font[i].binarypattern[j][k]) { image[j][k]=colorText; }
+                else { image[j][k]=colorBackground; }
+            }
+        }
+    }
+    else // caractère inexistant on le remplace par un glyphe inconnu
+    {
+        ShConvertCharacterToPattern(255,image,colorText,colorBackground);
+    }
+}
---
+++ old/tech153lab03.md
@@ -0,0 +1,25 @@
+# TECH 153 Lab 03
+
+- [ ] Demonstrate the execution of the program to your professor. ___/2
+
+## 1. Pick up your pi kit Tuesday or Wednesday before your lab
+Set up your pi and create a makefile on it as per [C15301PiLinux2025-09-05.docx](https://humberital-my.sharepoint.com/:w:/g/personal/mdrk0011_humber_ca/Efke6M89ofxLjnqyeB83baYBAkwiakz4cjQ-lWvGdJhaYQ?e=nWC3EL)
+
+## 2. Use WinSCP to copy compass.c from mikelake and put it onto your pi in your ceng153 folder
+Change   
+```
+#define HTS221 0
+#define LPS25H 0
+```
+to   
+```
+#define HTS221 1
+#define LPS25H 1
+```
+and run your code to get real readings.   
+## 3. Magnetic compass   
+contribute to arrow.h   
+compile and run arrow.c   
+Continue to create a program for providing the readings from the lsm9ds1 digital magnetic compass.   
+
+
